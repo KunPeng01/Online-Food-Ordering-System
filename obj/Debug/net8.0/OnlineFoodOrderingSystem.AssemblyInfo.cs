42029f7259d89087b22682c1e1d5bdbf648ffbb9
ykpfly <yangku@sheridancollege.ca>
2024-09-24 15:02:02 -0400

    project initalized

--- obj/Debug/net8.0/OnlineFoodOrderingSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineFoodOrderingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineFoodOrderingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5cc73e7a5371bc08b5934372cf00b1f5c7964ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d024587c96a68cbe5ddf411a2a31ca72fca6bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineFoodOrderingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineFoodOrderingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
