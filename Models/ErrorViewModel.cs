c5cc73e7a5371bc08b5934372cf00b1f5c7964ed
ykpfly <yangku@sheridancollege.ca>
2024-09-24 14:38:06 -0400

    Initial commit for Online Food Ordering System

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OnlineFoodOrderingSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
