622ae4848e6319adbce2a18119bf0fc1a58e86e2
tapa991 <tapa@sheridancollege.ca>
2024-11-03 01:32:12 -0400

    Added order page

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineFoodOrderingSystem.Data;
+
+namespace OnlineFoodOrderingSystem.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private OnlineFoodOrderDbContext _context;
+
+        public OrderController(OnlineFoodOrderDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
